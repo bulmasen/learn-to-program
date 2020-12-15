a8f0e4337d55ed925e9752168a9a909cc8fe81ec
Roman_Schmidt <teplo.doma@gmail.com>
2020-12-15 23:26:39 +0300

    Fix homeWork-lesson05_1.py

--- GB_LearnProgramming/Python_Programming/lesson-05/homeWork-lesson05_1.py
+++ GB_LearnProgramming/Python_Programming/lesson-05/homeWork-lesson05_1.py
@@ -3,7 +3,7 @@
 # строка.
 
 in_str = ' '
-with open('proc_file.txt', 'r') as proc_file:
+with open('proc_file.txt', 'w') as proc_file:
     while in_str:
         in_str = input('Введите строку для добавления в файл: ')
         proc_file.write(in_str + '\r')
