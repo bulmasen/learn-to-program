47beebf54f8ce0a4cc8b5c85084b11ea1dc4c7ba
Roman_Schmidt <teplo.doma@gmail.com>
2020-12-17 01:56:53 +0300

    Update homeWork-lesson05_2.py and homeWork-lesson05_4.py

--- GB_LearnProgramming/Python_Programming/lesson-05/homeWork-lesson05_4.py
+++ GB_LearnProgramming/Python_Programming/lesson-05/homeWork-lesson05_4.py
@@ -7,4 +7,25 @@
 # построчно данные. При этом английские числительные должны заменяться на
 # русские. Новый блок строк должен записываться в новый текстовый файл.
 
-translation = [['One', :]
+translation = {'one': 'один',
+               'two': 'два',
+               'three': 'три',
+               'four': 'четыре',
+               'five': 'пять',
+               'six': 'шесть',
+               'seven': 'семь',
+               'eight': 'восемь',
+               'nine': 'девять',
+               'ten': 'десять'
+               }
+
+with open('translated.txt', 'w') as translated:
+    with open('eng_strings.txt', 'r') as source:
+        for source_line in source:
+            translated_line = ''
+            for string in source_line.split():
+                if string.lower() not in translation.keys():
+                    translated_line += string + ' '
+                else:
+                    translated_line += translation.get(string.lower()) + ' '
+            translated.write(translated_line.capitalize().rstrip() + '\n')
