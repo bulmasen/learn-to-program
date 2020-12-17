0d90722c49e70693a400e16fc196dd1cd3586950
Roman_Schmidt <teplo.doma@gmail.com>
2020-12-17 15:22:26 +0300

    Update homeWork-lesson05_6.py

--- GB_LearnProgramming/Python_Programming/lesson-05/homeWork-lesson05_6.py
+++ GB_LearnProgramming/Python_Programming/lesson-05/homeWork-lesson05_6.py
@@ -18,7 +18,6 @@ with open('disciplines.txt', 'r') as source_file:
         for symbol in chunks[1]:
             if symbol in '0123456789 ':
                 digits += symbol
-        # subjects[chunks[0]] = sum(int(el) for el in digits.split())
         subjects[chunks[0]] = sum(int(el) for el in digits.split())
 
 print(subjects)
