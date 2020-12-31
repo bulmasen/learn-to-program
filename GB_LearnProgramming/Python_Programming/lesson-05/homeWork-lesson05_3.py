625e3be4cbaedfff3347d73cf2f15cdc11dc1ce6
Roman_Schmidt <teplo.doma@gmail.com>
2020-12-31 03:58:57 +0300

    Update homeWork-lesson05_3.py

--- GB_LearnProgramming/Python_Programming/lesson-05/homeWork-lesson05_3.py
+++ GB_LearnProgramming/Python_Programming/lesson-05/homeWork-lesson05_3.py
@@ -7,7 +7,7 @@
 # Петров 13749.32
 
 with open(r'salaries.txt', 'r', encoding='utf-8') as salaries:
-    non_rich = 'Сотрудники с доходом ниже 20,000.00:\n'
+    non_rich = ['Сотрудники с доходом ниже 20,000.00:\n']
     total_salary = float()
     total_employees = 0
     for line in salaries:
@@ -15,9 +15,9 @@ with open(r'salaries.txt', 'r', encoding='utf-8') as salaries:
         for word in line.split():
             employee.append(word)
         if float(employee[1]) < 20000:
-            non_rich += f'{employee[0]}: {float(employee[1]):,.2f}\n'
+            non_rich.append(f'{employee[0]}: {float(employee[1]):,.2f}\n')
         total_salary += float(employee[1])
         total_employees += 1
 
 print(f'Среднеарифметический доход сотрудников в списке: {total_salary / total_employees:,.2f}')
-print(non_rich)
+print(''.join(non_rich))
